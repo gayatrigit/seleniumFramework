2954527fda548bf2f593df9e2b4c45044c136e0c
gayatrigit <gayatri.charchi@gmail.com>
2026-07-14 17:22:43 +0530

    commiting old

--- src/test/java/com/crm/qa/tests/LoginPageTest.java
+++ src/test/java/com/crm/qa/tests/LoginPageTest.java
@@ -4,10 +4,12 @@ package com.crm.qa.tests;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.crm.qa.base.TestBase;
 import com.crm.qa.pages.LoginPage;
+import com.crm.qa.util.ExcelReader;
 import com.crm.qa.pages.HomePage;
 
 public class LoginPageTest extends TestBase{
@@ -17,7 +19,14 @@ public class LoginPageTest extends TestBase{
 	public LoginPageTest() {
 		super();
 	}
-
+@DataProvider
+public Object[][] getData()
+{
+	ExcelReader reader =
+          new ExcelReader(System.getProperty("user.dir"+" "+"/data"), "input.xlsx");
+	
+	return reader.getData("Login");
+}
 	@BeforeMethod
 	public void setUp() {
 	
@@ -36,7 +45,7 @@ public class LoginPageTest extends TestBase{
 			boolean flag = loginPage.validatecrmLogo();
 			Assert.assertTrue(flag);
 		}
-	@Test
+	@Test(dataProvider="getData")
 	public void loginTest() {
 		homePage=loginPage.login(prop.getProperty("username"),prop.getProperty("password"));
 	}
